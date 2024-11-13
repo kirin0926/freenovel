050f0723659d1f44664de972a037b6979b963cd0
novelshort <yq963657293@outlook.com>
2024-11-13 17:10:11 +0800

    固定化supabase参数

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL as string;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY as string;
+// const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL as string;
+// const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY as string;
+const supabaseUrl = 'https://upljbjvdxrnkpawobuhv.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVwbGpianZkeHJua3Bhd29idWh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAzNDA2ODMsImV4cCI6MjA0NTkxNjY4M30.r7nh29_4YgVXjKJ8BVQqadVunAOnI0MY-qrovTkpyWQ';
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 // 小说数据类型
